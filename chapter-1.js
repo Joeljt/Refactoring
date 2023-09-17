3d090952d8dcc0713f63b67b4f0f4483f5bfce81
Joseph_L <joseph_l@126.com>
2023-09-17 21:53:37 +0800

    Split Loop - 拆分循环, 将同一个循环中彼此不相干的业务充分解耦

--- chapter-1.js
+++ chapter-1.js
@@ -4,16 +4,18 @@ function statement (invoice, plays) {
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-
-    volumeCredits += volumeCreditsFor(perf);
-
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
+
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
-
+  return result;
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
@@ -55,6 +57,4 @@ function statement (invoice, plays) {
       }
       return result;
   }
-
-  return result;
 }
